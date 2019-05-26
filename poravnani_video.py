5bff48c6508357c8985d2a1c1ce2adcf12edf60f
traparije <traparije12345@gmail.com>
2019-05-26 23:31:21 +0200

    Porocilo

--- poravnani_video.py
+++ poravnani_video.py
@@ -5,30 +5,31 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import hist2d
 from plots_and_reads import quiverOnImage,optFlowColorVisualisation,genImgsIntoArray
 import numpy as np
-import scipy as si
 from HS_piramidna import HornSchunckPiramidna,bicubicInterpolateWarp
-invPar=np.array([0,0])
+#invPar=np.array([0,0])
 
 gen=genImgsIntoArray('C:/VideosRV/img','png',31)
 I0=next(gen)
-uskup=np.zeros(I0.shape,dtype=np.float32)
-vskup=np.zeros(I0.shape,dtype=np.float32)
+
 i=0
-parInv=np.array((0,0),dtype=np.float32)
-uMed=0
+#parInv=np.array((0,0),dtype=np.float32)
+uMed=0#hranita oceno za optični tok trenurne glede na prvo sliko v videu
 vMed=0
 for img in gen:
     i+=1
     print(i)
     I1=img
-    #u,v=HornSchunck(I0,I1,0.1,100)
-    #showImage(I0)
-    #showImage(I1)
+
+    #u,v=HornSchunck(I0,I1,0.1,100) klasična metoda se ne obnese, saj so premiki preveliki
+
     u,v=HornSchunckPiramidna(I0,I1,7,0.001,0.5,150,10,5)
-    # print(np.median(u.flatten())) #večina pikslov se verjetno premakne za toliko po x
-    # print(np.median(v.flatten())) #večina pikslov se verjerno premakne za toliko po y
-    quiverOnImage(u,v,I0,scale=1,step=10)
-    optFlowColorVisualisation(u,v,I0) 
+    
+    #quiverOnImage(u,v,I0,scale=1,step=10) #odkomentirajte če želite sprotni prikaz
+    #optFlowColorVisualisation(u,v,I0) #odkomentirajte če želite sprotni prikaz
+
+    '''
+    ena izmed idej: iz 2d histograma izločiti kateri koši so najbolj zastopani in to uporabiti za oceno premika slike.
+     Mediana opt toka se izkaže za boljšo rešitev
 
     #nbins=1000
     #razpon=(-15,15)
@@ -37,13 +38,19 @@ for img in gen:
     #najy,najx=int(najskup%nbins),int(najskup//nbins)
     #parInv += np.array([ razpon[0] + (razpon[1]-razpon[0])/nbins*(najx-1), razpon[0] + (razpon[1]-razpon[0])/nbins*(najy-1) ])
     #iBack=transformImage(I1,transAffine2D(iTrans = (-parInv[0],-parInv[1])))
-    uMed +=np.median(u.flatten())
-    vMed +=np.median(v.flatten())
-    UFix=uMed*np.ones(I1.shape)
-    VFix=vMed*np.ones(I1.shape)
+
+    '''
+    uMed +=np.median(u.flatten())#večina pikslov se verjetno premakne za toliko po x
+    vMed +=np.median(v.flatten())#večina pikslov se verjerno premakne za toliko po y
+    
+    #shranjevanje
+    
+    #UFix=uMed*np.ones(I1.shape)
+    #VFix=vMed*np.ones(I1.shape)
     #saveImage('C:/VideosRV/imgfixed{}'.format(i+1),bicubicInterpolateWarp(I1,uskup,vskup),'png')
     #saveImage('C:/VideosRV/imgfixed{}'.format(i+1),bicubicInterpolateWarp(I1,UFix,VFix),'png')
     #saveImage('C:/VideosRV/imgfixed{}'.format(i+1),iBack,'png')
+
     I0=I1
 
     
